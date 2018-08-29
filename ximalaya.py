52e32d9176276cc62932ad412bad6231008e1591
joelYing <39625260+joelYing@users.noreply.github.com>
2018-08-29 08:21:31 +0000

    Update ximalaya.py

--- ximalaya.py
+++ ximalaya.py
@@ -18,9 +18,9 @@ category_Url = 'https://www.ximalaya.com/category/'
 base_url = 'https://www.ximalaya.com'
 base_api = 'https://www.ximalaya.com/revision/play/tracks?trackIds='
 header = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36'
+    'User-Agent': 'your-user-agent'
 }
-client = pymongo.MongoClient(host='localhost', port=27017)
+client = pymongo.MongoClient(host='localhost', port=port)
 db = client['ximalaya']
 
 
